aeb8cf0b2d73498d6f005181e14846b23468ecc4
SantiagoUmpierrezDev <santiagoump986@gmail.com>
2022-09-28 14:20:03 -0300

    remodeling the design  and improving js functionality

--- src/app.js
+++ src/app.js
@@ -1,81 +1,57 @@
-let cart = []; 
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 let store = document.getElementById("store");
 
      let generateCards = () => {
         return (store.innerHTML = clothesData.map((i) => {
             let {id,name,price,img,alt} = i;
-            return `<article id="card__${id}" class="section__article">
+            let search = cart.find ((i) => i.id === id) || []
+            return `<article id="cart__${id}" class="store__article">
             <img
             class="article__img"
             src=${img}
             alt=${alt}
             />
-            <div class="article__div">
-            <p class="article__paragraph">${name}</p>
-            <button id="button__${id}" class="article__button">Add to cart</button>
+            <div class="productInformation">
+                <div class="article__div">
+                    <p class="article__paragraph">${name}</p>
+                    <p class="price__paragraph">$ ${price}</p>
+                </div>
+                <div class="store__div">
+                    <button class="cart__add" onClick="addToCart(${id})">Add to cart</button>
+                </div>
             </div>
-            <div>
-            <p class="price__paragraph">U$D ${price}</p>
-            </div>
-            </article>`;
+            </article>`
         }) 
         .join(""));
     };   
     
     generateCards();
 
-    let cartSection = document.getElementById("cart__section");
-
-    function generateCardsinCart() {
-        if(cart.length > 0) {
-            document.getElementById('cart__h2').innerHTML= "Your Cart";
-        }else {
-            document.getElementById('cart__h2').innerHTML= "Your cart is empty";
-        }
-        
-        cart__section.innerHTML = '';
-        cart.forEach((item) => {
-            cart__section.innerHTML +=         
-            `<article id="cart__${item.id}" class="section__article">
-            <img
-            class="article__img"
-            src=${item.img}
-            alt=${item.alt}
-            />
-            <div class="article__div">
-            <p class="article__paragraph">${item.name}</p>
-            <button id="buttonCart__${item.id}" class="article__button">Add to cart</button>
-            </div>
-            <div>
-            <p class="price__paragraph">U$D ${item.price}</p>
-            <button class="cart__remove" onClick="removeItemFromCart(${item.id})">Remove</button>
-            </div>
-            </article>`;
-        })
-    };
-    
-
 const add = document.querySelectorAll(".article__button")
-
-function removeItemFromCart(id) {
-    cart = cart.filter((item) => item.id !== id);
-    generateCardsinCart();
-}
-
-function clearCart() {
-    cart = [];
-    generateCardsinCart();
-}
 
 add.forEach(element => {
     element.addEventListener('click', function() {
         const clickedItem = (element.parentElement).parentElement;
         const clickedItemId = clickedItem.id.charAt(6);
         cart.push(clothesData[clickedItemId])
-        console.log(cart)
         generateCardsinCart()
     });
 });
+
+let addToCart = (id) => {
+    let selectedItem = id;
+    let search = cart.find((i) => i.id === selectedItem);
+    if (search === undefined) {
+        cart.push({
+        id: selectedItem,
+        amount: 1,
+    });
+    } else {
+        search.amount += 1;
+    }    
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 
 
---
+++ src/cart.js
@@ -0,0 +1,117 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart__section = document.getElementById('cart__section');
+let cart__items = document.getElementById('cart__items');
+
+function generateCardsinCart() {
+    if(cart.length > 0) {
+        return (cart__items.innerHTML = cart
+            .map((i) => {
+              let { id, amount } = i;
+              let search = clothesData.find((x) => x.id === id) || [];
+              return`<article id="cart__${id}" class="cart__article">
+              <img
+              class="article__img"
+              src=../${search.img}
+              alt=${search.alt}
+              />
+              <div class="productInformation">
+                <div class="article__div">
+                    <p class="article__paragraph">${search.name}</p>
+                    <p class="price__paragraph">$ ${search.price}</p>
+                </div>
+                <div class="inCart__div">
+                    <div class="quantity__div">
+                        <button class="plus-minus"><i onClick="decrement(${id})" class="fa-solid fa-minus"></i></button>
+                        <div id="${id}">${amount}</div>
+                        <button class="plus-minus"><i onclick="increment(${id})" class="fa-solid fa-plus"></i></button>
+                    </div>
+                </div>
+              </div>
+              <div class="remove__div">
+              <button class="cart__remove" onClick="removeItemFromCart(${id})">Remove</button>
+              </div>
+              </article>
+              `; })
+              .join(""));
+    }else {
+        cart__items.innerHTML = ``;
+        cart__section.innerHTML = `<h1 class="cart__h1">Your cart is empty</h1>
+        <a href="../index.html" class="cart__button">Back to the store</a>`;
+    }
+};
+
+generateCardsinCart ();
+
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = cart.find((i) => i.id === selectedItem);
+    if (search === undefined) {
+        cart.push({
+        id: selectedItem,
+        amount: 1,
+    });
+    } else {
+        search.amount += 1;
+    }    
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    amount(selectedItem);
+}
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = cart.find((i) => i.id === selectedItem);
+    if (search === undefined) return;
+    if (search.amount === 0) return;
+    else {
+        search.amount -= 1;
+    }    
+
+    amount(selectedItem);
+
+    cart = cart.filter ((i) => i.amount !== 0);
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+}
+
+let amount = (id) => {
+    let selectedItem = id;
+    let search = cart.find((i) => i.id === selectedItem);
+    document.getElementById(id).innerHTML = search.amount;
+    totalPrice();
+};
+
+function removeItemFromCart(id) {
+    cart = cart.filter((item) => item.id !== id);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    totalPrice();
+    generateCardsinCart();
+}
+
+function emptyCart() {
+    cart = [];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    generateCardsinCart();
+}
+
+let totalPrice = () => {
+    if (cart.length > 0) {
+      let price = cart
+        .map((i) => {
+          let { id, amount } = i;
+          let search = clothesData.find((x) => x.id === id) || [];
+          return amount * search.price;
+        })
+        .reduce((i, x) => i + x, 0);
+      cart__section.innerHTML = `
+      <h1 class="cart__h1">Cart total : $ ${price}</h1>
+      <button class="cart__button">Complete purchase</button>
+      <button onclick="emptyCart()" class="cart__button">Empty Cart</button>
+      `;
+    } else return;
+  };
+  
+  totalPrice();
